63b569d381690595340862a05860ab5ee5f00780
Levi Miller <paullevimiller@gmail.com>
2019-07-16 11:33:13 -0700

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+    # your code here!
+    i = 0
+    new_collection = Array.new 
+    while i < collection.length
+        if (yield collection[i]) == true
+            new_collection.push(collection[i])
+        end
+        i += 1
+    end
+    return new_collection
 end
